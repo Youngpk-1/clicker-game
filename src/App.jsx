6ec06270f7d86fbdf390921679205ded4bac8b54
isaacwatson30 <isaacwatson30@gmail.com>
2025-10-30 20:43:28 -0500

    added more js in app.jsx and css in the app.css file

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,32 @@ import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(3);
+  const [appleSize, setAppleSize] = useState(100);
 
   function clickTarget(event) {
     event.stopPropagation(); // Prevents triggering parent clicks
     setScore(score + 1);
+    randomeSize();
   }
 
   function missTarget() {
-  setLives(lives - 1)
-}
+    setLives(lives - 1);
+    randomeSize();
+  }
+
+  function randomNumber(a, b) {
+    return Math.floor(Math.random() * (b - a) + a);
+  }
+
+  function randomeSize() {
+    const randomSize = randomNumber(20, 100);
+    setAppleSize(randomSize);
+  }
+
+  const appleStyle = {
+    width: appleSize + "px",
+    height: appleSize + "px",
+  };
 
   return (
     <>
